0eea493adbfcefeba35e6411d23dbeecb05ce908
Tim <tim.mcmichael@estrellamountain.edu>
2024-12-10 14:07:34 -0700

    Ch 2a start files

---
+++ OOP with Java (CIS150AB)/02 Variables/DeclaringVariables.java
@@ -0,0 +1,18 @@
+/*
+ * This program demonstrates variable declaration statements, then
+ * outputs the stored values.
+ */
+
+public class DeclaringVariables {
+
+    public static void main(String[] args) {
+        int age;
+        double height;
+        String name;
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(name);
+    }
+
+}
