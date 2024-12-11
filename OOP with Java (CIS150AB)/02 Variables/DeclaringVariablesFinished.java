2dcedd6089549cb7be10f153d442cef2a76e13ad
Tim <tim.mcmichael@estrellamountain.edu>
2024-12-10 19:21:41 -0700

    "Using Variables" video

---
+++ OOP with Java (CIS150AB)/02 Variables/DeclaringVariablesFinished.java
@@ -0,0 +1,23 @@
+/*
+ * This program demonstrates variable declaration statements, then
+ * outputs the stored values.
+ */
+
+public class DeclaringVariablesFinished {
+
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 71.5;
+        String name = "David";
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(name);
+
+        age = 75;
+        System.out.println(age);
+
+        name = "Dave";
+    }
+
+}
