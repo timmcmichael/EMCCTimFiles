125bbbe5bbbee4c74beb8110ad8a615428a350c4
Tim McMichael <tim.mcmichael@estrellamountain.edu>
2025-02-01 10:18:18 -0700

    video files (constructors)

---
+++ OOP with Java (CIS150AB)/04 Classes and Objects/MobileCustomer.java
@@ -0,0 +1,74 @@
+/*
+ * CellCustomer
+ * This class represents a customer of a cell phone service company, focusing on the customer's data plan and usage.
+ */
+package module04;
+
+public class MobileCustomer {
+
+    // Fields (instance variables)
+    private String name;
+    private String phoneNumber;
+    private double monthlyData;
+    private double remainingData;
+
+    // Constructors
+    public MobileCustomer(String name, String phoneNumber, double monthlyData) {
+        this.name = name;       // name = name doesn't work right!
+        this.phoneNumber = phoneNumber;
+        this.monthlyData = monthlyData;
+        this.remainingData = monthlyData;
+    }
+
+    public MobileCustomer(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.monthlyData = 8.0;
+        this.remainingData = 8.0;
+    }
+
+    // Getters
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public double getMonthlyData() {
+        return this.monthlyData;
+    }
+
+    public double getRemainingData() {
+        return this.remainingData;
+    }
+
+    // Setters
+    public void setName(String cName) {
+        this.name = cName;
+    }
+
+    public void setPhoneNumber(String cPhoneNumber) {
+        this.phoneNumber = cPhoneNumber;
+    }
+
+    public void setMonthlyData(double cMonthlyData) {
+        this.monthlyData = cMonthlyData;
+        this.remainingData = cMonthlyData;
+    }
+
+    // Mutators
+    public void useData(double dataUsed) {
+        this.remainingData -= dataUsed;
+    }
+
+    public void addData(double amount) {
+        this.remainingData += amount;
+    }
+
+    public void resetData() {
+        this.remainingData = this.monthlyData;
+    }
+    
+}
---
+++ OOP with Java (CIS150AB)/04 Classes and Objects/MobileCustomerDemo.java
@@ -0,0 +1,40 @@
+/*
+ * CellCustomerDemo
+ * Tests and demonstrates functionality of the CellCustomer class by instantiating objects and using instance methods.
+ */
+package module04;
+
+public class MobileCustomerDemo {
+   
+    public static void main(String[] args) {
+        MobileCustomer customer1;
+        customer1 = new MobileCustomer("Deadpool", "800-867-5309", 16.0);
+        MobileCustomer customer2 = new MobileCustomer("Ryan Reynolds", "555-555-5555");
+
+        // customer1.setName("Deadpool");
+        // customer1.setPhoneNumber("800-867-5309");
+        // customer1.setMonthlyData(20.0);
+        // customer2.setName("Ryan Reynolds");
+
+        System.out.println(customer1.getName());
+        System.out.println(customer1.getPhoneNumber());
+        System.out.println(customer1.getRemainingData());
+        System.out.println(customer1.getMonthlyData());
+
+        customer1.useData(2.5);
+        customer1.addData(1.25);
+        System.out.println("Still remaining: " + customer1.getRemainingData());
+
+        customer1.resetData();
+        System.out.println("After reset: " + customer1.getRemainingData());
+
+        System.out.println("---------- customer2");
+        System.out.println(customer2.getName());
+        System.out.println(customer2.getPhoneNumber());
+        System.out.println(customer2.getRemainingData());
+        System.out.println(customer2.getMonthlyData());
+
+    }
+
+
+}
