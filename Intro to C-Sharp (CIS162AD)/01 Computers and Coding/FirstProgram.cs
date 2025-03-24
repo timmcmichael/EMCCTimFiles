c03acb5c343dda45ab0f8b5d60e7f842b2b69154
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-03-24 13:56:52 -0700

    Sample files from video

---
+++ Intro to C-Sharp (CIS162AD)/01 Computers and Coding/FirstProgram.cs
@@ -0,0 +1,10 @@
+﻿/*
+ * This is a multi-line comment. The compiler will ignore it.
+ *
+ * You should follow the directions in Canvas for this block.
+ * 
+ */
+
+// This is a single-line comment!
+Console.WriteLine("Hello, C#!"); // My first line of code!
+Console.Write("Welcome to CIS162AD!");
