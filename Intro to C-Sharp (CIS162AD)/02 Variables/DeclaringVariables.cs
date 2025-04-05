885ae6f3e4247b061720e0c33ae2a7e817402fb5
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-04-05 08:20:42 -0700

    Video example file

---
+++ Intro to C-Sharp (CIS162AD)/02 Variables/DeclaringVariables.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * This program demonstrates variable declaration statements, then
+ * outputs the stored values.
+ */
+
+int age;
+double height;
+string name;
+
+age = 25;
+height = 71.5;
+name = "David";
+
+Console.WriteLine(age);
+Console.WriteLine(height);
+Console.WriteLine(name);
+
+age = 75;
+Console.WriteLine(age);
+
+name = "Dave";
