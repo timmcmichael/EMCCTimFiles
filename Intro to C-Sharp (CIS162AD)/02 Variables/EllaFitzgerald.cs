5ad0f8b935db176b98ed95503434c88188edf8b3
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-04-11 10:57:44 -0700

    Add files via upload

---
+++ Intro to C-Sharp (CIS162AD)/02 Variables/EllaFitzgerald.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * This program outputs a few basic facts about Ella Fitzgerald.
+ *
+ * It demonstrates "string interpolation" to easily output variable values, and it shows how to use an
+ * "escape sequence."
+ */
+ 
+ string name = "Ella Fitzgerald";
+ int birthYear = 1917;
+ int deathYear = 1996;
+
+ int ageAtDeath = deathYear - birthYear;
+ 
+ Console.WriteLine($"{name} was born in {birthYear} and died in {deathYear}.");
+ Console.WriteLine($"She was {ageAtDeath} years old at the time of her death.");
+ Console.WriteLine("She was known as the \"First Lady of Song\"");
+ Console.WriteLine("She is considered one of the\nbest scat singers of all time.");
