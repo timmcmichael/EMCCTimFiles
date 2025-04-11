6353da9c2dd1fb41c8956084379197c28e69aa36
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-04-11 16:41:16 -0700

    Create Operations.cs

---
+++ Intro to C-Sharp (CIS162AD)/02 Variables/Operations.cs
@@ -0,0 +1,15 @@
+/*
+ * This program demonstrates casting to calculate an accurate average.
+ *
+ */
+
+int ann = 15;
+int deidre = 17;
+int vanessa = 17;
+int numFriends = 3;
+int totalAges = ann + deidre + vanessa;
+double avgAge = totalAges / (double)numFriends;
+Console.WriteLine("Average =" + avgAge);
+
+int modulo = 10 % 6;
+Console.WriteLine(modulo);
