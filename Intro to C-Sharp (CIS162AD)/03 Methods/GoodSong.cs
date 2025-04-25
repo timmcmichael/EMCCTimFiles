d586df047299708d725a235427d92240630ea9f4
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-04-25 12:16:12 -0700

    Source code from video

---
+++ Intro to C-Sharp (CIS162AD)/03 Methods/GoodSong.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * 
+ * This program outputs the lyrics of a good--but repetitive--song.
+ * 
+ * It demonstrates defining and calling a method.
+ * 
+ */
+
+namespace GoodSong
+{
+    internal class GoodSong
+    {
+        static void Main(string[] args)
+        {
+            OutputTitle();
+            Console.WriteLine("by The Kinks\n\n");
+            OutputChorus();
+            OutputChorus();
+            OutputChorus();
+        }
+
+        public static void OutputChorus()
+        {
+            Console.WriteLine("Yeah, you really got my now\nYou got me so I don't know what I'm doin', now");
+            Console.WriteLine("Oh yeah, you really got me now\nYou got me so I can't sleep at night");
+            //Console.WriteLine("You really got me\nYou really got me");
+            OutputTitle();
+            OutputTitle();
+        }
+
+        public static void OutputTitle()
+        {
+            Console.WriteLine("You Really Got Me");
+        }
+
+    }
+}
