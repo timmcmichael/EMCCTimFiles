c01792fe3dc3b4d55e84fab6ea472ba77b9c87fa
timmcmichael <50641864+timmcmichael@users.noreply.github.com>
2025-04-29 17:01:00 -0700

    Video example file

---
+++ Intro to C-Sharp (CIS162AD)/03 Methods/AreaOfOvalReturn.cs
@@ -0,0 +1,24 @@
+/*
+ * 
+ * Demo file
+ * 
+ * The demonstrates passing a return value from a method by implementing a method 
+ * that accepts two radii of and and calculates the area. 
+ * 
+ * Test Cases:
+ * 1.0, 1.0 -> 3.14
+ * 4.0, 12.0 -> 150.7964
+ * 15.67, 6.34 -> 312.1103
+ * 
+ */
+public class AreaOfOvalFinished {
+    public static void Main(String[] args) {
+        Console.Write( AreaOfOval(2.3, 4.5) );
+    }
+
+    public static void PrintAreaOfOval(double smallRadius,
+            double bigRadius) {
+        double area = 3.14 * smallRadius * bigRadius;
+        return area;
+    }
+}
