ff9880152eb1c5f1aaacddf323937c3f5386137c
Tim McMichael <50641864+timmcmichael@users.noreply.github.com>
2025-05-16 19:27:34 -0700

    Create PearlJamDebut.cs

---
+++ Intro to C-Sharp (CIS162AD)/05 Loops/PearlJamDebut.cs
@@ -0,0 +1,15 @@
+/*
+ * Quizzes the user on the name of the first Pearl Jam album.
+ * 
+ */
+
+const string FirstAlbum = "Ten";
+string guess = "";
+
+Console.Write("What is the title of Pearl Jam's debut album? ");
+guess = Console.ReadLine();
+
+if (guess != FirstAlbum)
+{
+    Console.WriteLine("Wrong!");
+}
