001ac51a8b167cb3319184337154e62e25383d79
Tim McMichael <50641864+timmcmichael@users.noreply.github.com>
2025-05-16 19:24:14 -0700

    Update WhileLoopDemo2.java

--- OOP with Java (CIS150AB)/06 Loops/WhileLoopDemo2.java
+++ OOP with Java (CIS150AB)/06 Loops/WhileLoopDemo2.java
@@ -6,20 +6,10 @@ public class WhileLoopDemo2 {
 
     public static void main(String[] args) {
         // initialize control variable
-        int counter = 0;
 
         // check the boolean expression
-        while (counter < 10) {
-            // update the control variable
-            System.out.println(++counter);
 
-        }
-
+        // update the control variable
     }
 
 }
-
-// Starts at 0 and ends at 9
-// add one to count in the println()
-// What happens if you get your relational sign wrong?
-// What happens if you put a semicolon at the end of your while statement?
