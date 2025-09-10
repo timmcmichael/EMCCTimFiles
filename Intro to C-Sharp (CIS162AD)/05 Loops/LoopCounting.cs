67cdfcdbccb30cdeb009118a5d731e772a7cd27a
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-09-10 11:38:51 -0700

    Create LoopCounting.cs for loop counting example
    Add LoopCounting.cs to demonstrate loop counting.

---
+++ Intro to C-Sharp (CIS162AD)/05 Loops/LoopCounting.cs
@@ -0,0 +1,15 @@
+/*
+ * This program counts using loops!
+ * 
+ * It demonstrates the use of definite loops using while and do-while.
+ * 
+ */ 
+
+// Initialize control variable
+
+
+// Check the control variable
+
+
+// Update control variable
+
