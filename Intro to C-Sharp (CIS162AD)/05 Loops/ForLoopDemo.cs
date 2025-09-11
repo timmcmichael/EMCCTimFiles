6829dc63cbe45bcbe1a3e84e5cc99104d192d337
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-09-11 12:25:48 -0700

    Add ForLoopDemo.cs to demonstrate for loop syntax
    This code demonstrates the syntax of a for loop using a while loop.

---
+++ Intro to C-Sharp (CIS162AD)/05 Loops/ForLoopDemo.cs
@@ -0,0 +1,15 @@
+/*
+ * This code demonstrates the syntax of a for loop.
+ * 
+ */ 
+
+
+// Initialize control variable
+int counter = 1;
+
+// Check the control variable (Boolean expression)
+while (counter <= 10)
+{
+    Console.WriteLine(counter);
+    counter++; // Update the control variable
+}
