93a77343232129bb7d55f8fd0c05a38eb54fa2a2
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-09-15 11:55:32 -0700

    Add ForLoopDemoFinished.cs to demonstrate for loop
    This code demonstrates a for loop that counts down from 20 to 1 in decrements of 4, printing each value.

---
+++ Intro to C-Sharp (CIS162AD)/05 Loops/ForLoopDemoFinished.cs
@@ -0,0 +1,26 @@
+/*
+ * This code demonstrates the syntax of a for loop.
+ * 
+ */ 
+
+for (int counter = 20; counter >= 1; counter -= 4)
+{
+    Console.WriteLine(counter);
+}
+
+// Basic 1 to 10
+//for (int counter = 1; counter <= 10; counter++)
+//{
+//    Console.WriteLine(counter);
+//}
+
+
+//// Initialize control variable
+//int counter = 1;
+
+//// Check the control variable (Boolean expression)
+//while (counter <= 10)
+//{
+//    Console.WriteLine(counter);
+//    counter++; // Update the control variable
+//}
