61e3f01a5c975754444d3043376674db4a0ffa0f
Tim McMichael <50641864+timmcmichael@users.noreply.github.com>
2025-09-22 19:58:23 -0700

    Create EvilDemo.cs

---
+++ Intro to C-Sharp (CIS162AD)/05 Loops/EvilDemo.cs
@@ -0,0 +1,12 @@
+/*
+ * Just don't. You're better than this
+ */
+
+for (int counter = 1; counter <= 10; counter++) 
+{
+    Console.WriteLine(counter);
+    if (counter == 5) 
+    {
+        counter = 11;
+    }
+}
