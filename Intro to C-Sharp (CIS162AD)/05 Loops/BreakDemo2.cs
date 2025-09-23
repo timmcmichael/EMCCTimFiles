135964890ae6f2add529e1ca3cbde347aa2f2741
Tim McMichael <50641864+timmcmichael@users.noreply.github.com>
2025-09-22 19:56:48 -0700

    Create BreakDemo2.cs

---
+++ Intro to C-Sharp (CIS162AD)/05 Loops/BreakDemo2.cs
@@ -0,0 +1,16 @@
+/*
+ * I can't even bring myself to explain what this does...
+ */
+
+
+int counter = 1;
+
+while (true) 
+{
+    Console.WriteLine(counter);
+    counter++;
+    if (counter > 10) 
+    {
+        break;
+    }
+}
