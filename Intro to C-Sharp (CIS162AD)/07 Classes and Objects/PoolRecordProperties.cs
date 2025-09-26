bd908a2372acd119f12632d3d7a649729e411f03
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-09-25 19:18:12 -0700

    Add PoolRecord class for pool water records using properties
    This class models pool water records for a single day using properties for date, water temperature, and pH level.

---
+++ Intro to C-Sharp (CIS162AD)/07 Classes and Objects/PoolRecordProperties.cs
@@ -0,0 +1,38 @@
+/*
+ * PoolRecordDemo
+ *
+ * Models a record off pool water records for a single day, using properties.
+ *
+ * This class works with the version of the PoolRecordDemo program found in PoolRecordDemoProperties.cs.
+ *
+ */
+public class PoolRecord
+{
+    // Fields
+    private string date;
+    private int waterTemp;
+    private double phLevel;
+
+    // Properties
+    public string Date
+    {
+        get { return this.date; }
+        set { this.date = value; }
+    }
+    public int WaterTemp
+    {
+        get { return this.waterTemp; }
+        set { this.waterTemp = value; }
+    }
+    public double PhLevel
+    {
+        get { return this.phLevel; }
+        set 
+        { 
+            if (value >= 0.0 && value <= 14.0)
+            {
+                this.phLevel = value; 
+            }
+        }
+    }
+}
