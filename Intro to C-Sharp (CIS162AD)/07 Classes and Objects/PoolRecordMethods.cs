096434e6003c100f26fd2e8f6b55053a8d52c596
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-09-25 19:40:11 -0700

    Add constructors to PoolRecord class
    Added constructors to initialize PoolRecord with default values or a specified date.

--- Intro to C-Sharp (CIS162AD)/07 Classes and Objects/PoolRecordMethods.cs
+++ Intro to C-Sharp (CIS162AD)/07 Classes and Objects/PoolRecordMethods.cs
@@ -13,6 +13,21 @@ public class PoolRecord
     private int waterTemp;
     private double phLevel;
 
+    // Constructors
+    public PoolRecord()
+    {
+        this.Date = "2025-01-01";
+        this.WaterTemp = 0;
+        this.PhLevel = 0.0;
+    }
+    
+    public PoolRecord(string date)
+    {
+        this.Date = date;
+        this.WaterTemp = 0;
+        this.PhLevel = 0.0;
+    }
+
     // Getters
     public string GetDate()
     {
