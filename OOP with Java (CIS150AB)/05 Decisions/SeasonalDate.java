ff8a5da65af4c2f49393e131b8f51a15691ee599
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-09-29 11:06:57 -0700

    Create SeasonalDate class with season determination
    Implemented SeasonalDate class with fields for day, month, and year, including a constructor and get methods. Added a method to determine the season based on specified date ranges.

---
+++ OOP with Java (CIS150AB)/05 Decisions/SeasonalDate.java
@@ -0,0 +1,29 @@
+package module05;
+
+/*
+ * Solution Walkthrough
+ * Module 05
+ * 
+ * Create a class called SeasonalDate (in SeasonalDate.java) that includes separate fields for a day, 
+ * month, and year; include get methods for each field. A constructor should accept values for each
+ * of the fields and assign them appropriately. A method called getSeason() returns a String
+ * indicating the astronomical season for the date using the ranges below:
+ *  Spring starts March 20
+ *  Summer starts June 21
+ *  Fall starts September 23
+ *  Winter starts December 21
+ *    Note: These dates are based on equinoxes and solstices for 2023; for simplicity,
+ *    your code can use these same dates regardless of the year.
+ * Next, create a program in TestSeasonalDate.java that demonstrates SeasonalDate by creating multiple
+ * instances of the class and outputting the seasons. This program does not require user input.
+ * 
+ * Test Cases:
+ * 3/1/2023 -> Winter
+ * 3/20/2023 -> Spring
+ * 6/28/2023 -> Summer
+ * 10/1/2022 -> Fall
+ */
+
+public class SeasonalDate {
+    
+}
