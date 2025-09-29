9db16cf6ecd190f96e1a7424bee98df53f1e38e5
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-09-29 11:15:05 -0700

    Create TestSeasonalDate.java

---
+++ OOP with Java (CIS150AB)/05 Decisions/TestSeasonalDate.java
@@ -0,0 +1,12 @@
+package module05;
+
+public class TestSeasonalDate {
+    public static void main(String[] args) {
+        SeasonalDate sd1 = new SeasonalDate(3, 1, 2023);
+        SeasonalDate sd2 = new SeasonalDate(3, 20, 2023);
+        SeasonalDate sd3 = new SeasonalDate(6, 28, 2023);
+        SeasonalDate sd4 = new SeasonalDate(10, 1, 2022);
+
+    }
+
+}
