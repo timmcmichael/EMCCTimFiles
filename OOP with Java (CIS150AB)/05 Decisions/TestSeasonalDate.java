4d7c00a3a80657c9ffa677b6f229f9bfe64bc63e
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-09-29 11:27:03 -0700

    Print seasonal dates for multiple SeasonalDate instances
    Added output statements to display seasonal dates.

--- OOP with Java (CIS150AB)/05 Decisions/TestSeasonalDate.java
+++ OOP with Java (CIS150AB)/05 Decisions/TestSeasonalDate.java
@@ -1,5 +1,3 @@
-package module05;
-
 public class TestSeasonalDate {
     public static void main(String[] args) {
         SeasonalDate sd1 = new SeasonalDate(3, 1, 2023);
@@ -7,6 +5,17 @@ public class TestSeasonalDate {
         SeasonalDate sd3 = new SeasonalDate(6, 28, 2023);
         SeasonalDate sd4 = new SeasonalDate(10, 1, 2022);
 
+        String dateStr = sd1.getMonth() + "/" + sd1.getDay() + "/" + sd1.getYear();
+        System.out.println(dateStr + " is in " + sd1.getSeason());
+
+        dateStr = sd2.getMonth() + "/" + sd2.getDay() + "/" + sd2.getYear();
+        System.out.println(dateStr + " is in " + sd2.getSeason());
+
+        dateStr = sd3.getMonth() + "/" + sd3.getDay() + "/" + sd3.getYear();
+        System.out.println(dateStr + " is in " + sd3.getSeason());
+
+        dateStr = sd4.getMonth() + "/" + sd4.getDay() + "/" + sd4.getYear();
+        System.out.println(dateStr + " is in " + sd4.getSeason());
     }
 
 }
