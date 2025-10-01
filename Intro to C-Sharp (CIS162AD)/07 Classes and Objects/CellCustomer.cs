1261efc58bae1e28333963bce03d3fe08a8b5bfb
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-10-01 12:45:52 -0700

    Add files via upload

---
+++ Intro to C-Sharp (CIS162AD)/07 Classes and Objects/CellCustomer.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * CellCustomer
+ * 
+ * This class represents a customer of a cell phone service company, focusing on the 
+ * customer's data plan and usage.
+ * 
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    internal class CellCustomer
+    {
+
+        // Fields (instance variables)
+        private string name;
+        private string phoneNumber;
+        private double monthlyData;
+        private double remainingData;
+
+        // Getters
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetPhoneNumber()
+        {
+            return this.phoneNumber;
+        }
+
+        public double GetMonthlyData()
+        {
+            return this.monthlyData;
+        }
+
+        public double GetRemainingData()
+        {
+            return this.remainingData;
+        }
+
+        // Setters
+        public void SetName(string cName)
+        {
+            this.name = cName;
+        }
+
+        public void SetPhoneNumber(string cPhoneNumber)
+        {
+            this.phoneNumber = cPhoneNumber;
+        }
+
+        public void SetMonthlyData(double cMonthlyData)
+        {
+            this.monthlyData = cMonthlyData;
+            this.remainingData = cMonthlyData;
+        }
+
+        // Mutators
+
+        public void UseData(double dataUsed)
+        {
+            this.remainingData -= dataUsed;
+        }
+
+        public void AddData(double amount)
+        {
+            this.remainingData += amount;
+        }
+
+        public void ResetData()
+        {
+            this.remainingData = this.monthlyData;
+        }
+
+    }
+}
---
+++ Intro to C-Sharp (CIS162AD)/07 Classes and Objects/CellCustomerDemo.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * CellCustomerDemo
+ * 
+ * Tests and demonstrates functionality of the CellCustomer class by instantiating objects
+ * and using their instance methods.
+ * 
+ */
+
+using OOPDemo;
+
+CellCustomer customer1;
+customer1 = new CellCustomer();
+CellCustomer customer2 = new CellCustomer();
+
+customer1.SetName("Deadpool");
+customer1.SetPhoneNumber("800-867-5309");
+customer1.SetMonthlyData(20.0);
+customer2.SetName("Ryan Reynolds");
+
+Console.WriteLine(customer1.GetName());
+Console.WriteLine(customer1.GetPhoneNumber());
+Console.WriteLine(customer1.GetRemainingData());
+Console.WriteLine(customer2.GetMonthlyData());
+
+customer1.UseData(2.5);
+customer1.AddData(1.25);
+Console.WriteLine($"Remaining data: {customer1.GetRemainingData()}");
+
+customer1.ResetData();
+Console.WriteLine($"After reset: {customer1.GetRemainingData()}");
+
+Console.WriteLine("----------------");
+
+Console.WriteLine(customer2.GetName());
+
