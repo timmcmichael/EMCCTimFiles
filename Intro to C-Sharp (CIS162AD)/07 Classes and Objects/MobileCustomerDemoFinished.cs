ee56f5c7f1de8af8a83ff0dc6c054b0324780497
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-10-20 11:56:44 -0700

    Add files via upload

---
+++ Intro to C-Sharp (CIS162AD)/07 Classes and Objects/MobileCustomerDemoFinished.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * MobileCustomerDemo
+ * 
+ * Tests and demonstrates functionality of the CellCustomer class by instantiating objects
+ * and using their instance methods.
+ * 
+ * NOTE: This is a copy of the CellCustomerDemo class, renamed for the video on constructors.
+ * 
+ */
+
+using OOPDemo;
+
+MobileCustomer customer1;
+customer1 = new MobileCustomer("Deadpool", "800-867-5309", 16.0);
+MobileCustomer customer2 = new MobileCustomer("Ryan Reynolds", "555-555-5555");
+
+//customer1.SetName("Deadpool");
+//customer1.SetPhoneNumber("800-867-5309");
+//customer1.SetMonthlyData(20.0);
+//customer2.SetName("Ryan Reynolds");
+
+Console.WriteLine(customer1.GetName());
+Console.WriteLine(customer1.GetPhoneNumber());
+Console.WriteLine(customer1.GetRemainingData());
+Console.WriteLine(customer2.GetMonthlyData());
+
+customer1.UseData(2.5);
+customer1.AddData(1.25);
+Console.WriteLine($"Remaining data: {customer1.GetRemainingData()}");
+
+customer1.ResetData();
+Console.WriteLine($"After reset: {customer1.GetRemainingData()}");
+
+Console.WriteLine("----------------");
+Console.WriteLine("Customer 2: ");
+Console.WriteLine(customer2.GetName());
+Console.WriteLine(customer2.GetPhoneNumber());
+Console.WriteLine(customer2.GetRemainingData());
+Console.WriteLine(customer2.GetMonthlyData());
---
+++ Intro to C-Sharp (CIS162AD)/07 Classes and Objects/MobileCustomerFinished.cs
@@ -0,0 +1,101 @@
+﻿/*
+ * MobileCustomer
+ * 
+ * This class represents a customer of a cell phone service company, focusing on the 
+ * customer's data plan and usage.
+ * 
+ *  * NOTE: This is a copy of the CellCustomer class, renamed for the video on constructors.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    internal class MobileCustomer
+    {
+
+        // Fields (instance variables)
+        private string name;
+        private string phoneNumber;
+        private double monthlyData;
+        private double remainingData;
+
+        // Constructor
+        public MobileCustomer(String name, String phoneNumber, double monthlyData)
+        {
+            this.name = name; // name = name doesn't work!
+            this.phoneNumber = phoneNumber;
+            this.monthlyData = monthlyData;
+            this.remainingData = monthlyData;
+        }
+
+        public MobileCustomer(String name, String phoneNumber)
+        {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+            this.monthlyData = 8.0;
+            this.remainingData = 8.0;
+        }
+
+        // Getters
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetPhoneNumber()
+        {
+            return this.phoneNumber;
+        }
+
+        public double GetMonthlyData()
+        {
+            return this.monthlyData;
+        }
+
+        public double GetRemainingData()
+        {
+            return this.remainingData;
+        }
+
+        // Setters
+        public void SetName(string cName)
+        {
+            this.name = cName;
+        }
+
+        public void SetPhoneNumber(string cPhoneNumber)
+        {
+            this.phoneNumber = cPhoneNumber;
+        }
+
+        public void SetMonthlyData(double cMonthlyData)
+        {
+            this.monthlyData = cMonthlyData;
+            this.remainingData = cMonthlyData;
+        }
+
+        // Mutators
+
+        public void UseData(double dataUsed)
+        {
+            this.remainingData -= dataUsed;
+        }
+
+        public void AddData(double amount)
+        {
+            this.remainingData += amount;
+        }
+
+        public void ResetData()
+        {
+            this.remainingData = this.monthlyData;
+        }
+
+    }
+}
