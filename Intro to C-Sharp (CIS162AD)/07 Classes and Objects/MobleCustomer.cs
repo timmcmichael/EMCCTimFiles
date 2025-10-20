92dc3bc70dcc48cfec3e19082488e12deef5d92d
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-10-20 09:39:11 -0700

    Add MobileCustomer class for managing mobile data
    This class manages customer data for a mobile service, including data usage and management.

---
+++ Intro to C-Sharp (CIS162AD)/07 Classes and Objects/MobleCustomer.cs
@@ -0,0 +1,84 @@
+/*
+ * MobileCustomer
+ * 
+ * This class represents a customer of a cell phone service company, focusing on the 
+ * customer's data plan and usage.
+ * 
+ *  * NOTE: This is a copy of the CellCustomer class, renamed for the video on constructors.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    internal class MobileCustomer
+    {
+
+        // Fields (instance variables)
+        private string name;
+        private string phoneNumber;
+        private double monthlyData;
+        private double remainingData;
+
+        // Getters
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetPhoneNumber()
+        {
+            return this.phoneNumber;
+        }
+
+        public double GetMonthlyData()
+        {
+            return this.monthlyData;
+        }
+
+        public double GetRemainingData()
+        {
+            return this.remainingData;
+        }
+
+        // Setters
+        public void SetName(string cName)
+        {
+            this.name = cName;
+        }
+
+        public void SetPhoneNumber(string cPhoneNumber)
+        {
+            this.phoneNumber = cPhoneNumber;
+        }
+
+        public void SetMonthlyData(double cMonthlyData)
+        {
+            this.monthlyData = cMonthlyData;
+            this.remainingData = cMonthlyData;
+        }
+
+        // Mutators
+
+        public void UseData(double dataUsed)
+        {
+            this.remainingData -= dataUsed;
+        }
+
+        public void AddData(double amount)
+        {
+            this.remainingData += amount;
+        }
+
+        public void ResetData()
+        {
+            this.remainingData = this.monthlyData;
+        }
+
+    }
+}
