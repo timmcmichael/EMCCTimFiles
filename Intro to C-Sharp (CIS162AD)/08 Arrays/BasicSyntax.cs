47e4cadf1043c7299cd4e31542680fd22ce6ef7c
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-10-31 10:47:59 -0700

    Add files via upload

---
+++ Intro to C-Sharp (CIS162AD)/08 Arrays/BasicSyntax.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Declaring arrays and assigning values with index numbers
+ * Retrieving and changing array values
+ * Initialization lists
+ *
+ * Team example from video (John, Marie, Danielle, Daveed, Javier, Vanessa)
+ *
+ */
+
+// string[] team = new string[6];
+//
+// team[0] = "John";
+// team[1] = "Marie";
+// team[3] = "Daveed";
+//
+// team[0] = "Someone other than John";
+
+string[] team = { "John", "Marie", "Danielle", "Daveed", "Javier", "Vanessa" };
+double[] prices = { 1.59, 3.99, 6.50 };
+
+Console.WriteLine(team[0]);
+Console.WriteLine(prices[0]);
+
+prices[0] += .50;
+Console.WriteLine(prices[0]);
+
+// Console.WriteLine(team[6].ToUpper());
