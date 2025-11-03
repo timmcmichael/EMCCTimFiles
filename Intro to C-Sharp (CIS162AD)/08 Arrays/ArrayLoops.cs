f8f832b9ad1fa9d094be4990c0a9dace99aec363
Tim McMichael <54223159+timvq68551@users.noreply.github.com>
2025-11-03 13:14:58 -0700

    Create ArrayLoops.cs for array loop demonstrations
    Added ArrayLoops.cs to demonstrate various array traversal techniques.

---
+++ Intro to C-Sharp (CIS162AD)/08 Arrays/ArrayLoops.cs
@@ -0,0 +1,12 @@
+/*
+ * Demonstration of using loops with arrays:
+ * Output traversal
+ * Nonstandard traversals
+ * Accumulation traversal (every other number?)
+ * Searching an array
+ * Input traversal
+ *
+ *
+ */
+String[] team = { "John", "Marie", "Danielle", "Daveed", "Javier", "Vanessa" };
+double[] prices = { 1.99, 2.25, 2.99, 9.99, 4.49 };
